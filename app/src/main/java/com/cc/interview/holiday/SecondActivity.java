22bf7da0c85a9b0d55ff7a82acec7d9d919d067e
XiaoJiu <417987689@qq.com>
2018-11-19 11:10:13 +0800

    git 命令行端代码修改提交

---
+++ app/src/main/java/com/cc/interview/holiday/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.cc.interview.holiday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        init();
+    }
+
+    private void init() {
+        //init
+    }
+
+
+}
