b4be074638a8ebb05621880375f72c812e0f34c5
XiaoJiu <417987689@qq.com>
2018-11-19 16:38:39 +0800

    second

--- app/src/main/java/com/cc/interview/holiday/SecondActivity.java
+++ app/src/main/java/com/cc/interview/holiday/SecondActivity.java
@@ -2,18 +2,27 @@ package com.cc.interview.holiday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends BaseActivity {
+
+    private View activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         init();
+        initData();
+    }
+
+    private void initData() {
+
     }
 
     public void init() {
         //init
+        activity_main=findViewById(R.id.activity_second);
     }
 
 
