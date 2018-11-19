a5dc000e208733dd8c954be2747e4793e91e85d1
XiaoJiu <417987689@qq.com>
2018-11-19 16:52:32 +0800

    third 用于远程second的merge测试

--- app/src/main/java/com/cc/interview/holiday/SecondActivity.java
+++ app/src/main/java/com/cc/interview/holiday/SecondActivity.java
@@ -3,10 +3,12 @@ package com.cc.interview.holiday;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends BaseActivity {
 
     private View activity_main;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,8 @@ public class SecondActivity extends BaseActivity {
     public void init() {
         //init
         activity_main=findViewById(R.id.activity_second);
+        tv= (TextView) findViewById(R.id.tv);
+        tv.setText("测试merge");
     }
 
 
